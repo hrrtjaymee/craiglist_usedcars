b6143c49018499a5330a43306f145716691970be
hrrtjaymee <harrietregencia@gmail.com>
2026-03-31 01:07:04 +0800

    feat: added a clean_title function

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import numpy as np
 from src.extract import get_data
+from src.transform import clean_batch
+import re
 
-'''
 ## INITIAL VIEW ON DATA ##
-path = os.getcwd()
-data = pd.read_csv(f'{path}/../used_cars_craiglist.csv', nrows = 100)
+# from pathlib import Path
+# ROOT = Path(__file__).resolve().parents[1]
+# data = pd.read_csv(f'{ROOT}/used_cars_craiglist.csv')
 # print(data.info())
 # print(data.describe())
-[print(x) for x in data['priceinfo'][2:76]]
+# [print(x) for x in data['label'].unique()]
+
+# for x in data['label']:
+    # match = re.search(r'\b(\d{4})\b', x)
+    # print(match.group())
 
 ## all columsn are TYPE STR
 ## assume all columns have missing values
@@ -19,9 +25,15 @@ data = pd.read_csv(f'{path}/../used_cars_craiglist.csv', nrows = 100)
 ## [result-posted-date] = how recent the posting was (in hrs based on when the data was retrieved)
 ## [location] = location (contains incorrect values)
 ## [priceinfo] = selling price (in US dollars)
-'''
 
 def main():
     ##hore: add db connection function
     BATCHSIZE = 100
     data = get_data(BATCHSIZE)
+    for batch, chunk in enumerate(data, start=1):
+        print(f'PROCESSING BATCH NUMBER {batch} | READ {len(chunk)} ROWS')
+        cleaned_batch = clean_batch(chunk)
+
+        print(cleaned_batch)
+
+main()
--- src/transform/transform.py
+++ src/transform/transform.py
@@ -1,10 +1,39 @@
 import pandas as pd
 import numpy as np
-
-
+import re
+from datetime import datetime
 
 #add data to database by batch (100 rows per batch)
-##rename columns
+##rename columns /
 #clean data
 
 #separate model year and car model in label column
+MIN_YEAR = 1950
+CURR_YEAR = datetime.today().year
+
+CAR_MODELS = {'chevrolet', 'ford', 'honda', 'acura', 'gmc',
+            'toyota', 'mercedes', 'subaru', 'nissan', 'chrysler',
+            'hyundai', 'corvette', 'volkswagen', 'mazda', 'land rover',
+            'audi', 'kia', 'dodge', 'tesla','bmw', 'jeep',
+            'lexus', 'porsche', 'austin', 'chevy'}
+#todo: clean titles so that the capitalization is uniform & chevy == chevrolet
+def clean_batch(data: pd.DataFrame):
+    clean_title(data)
+
+    final_data = data[['year', 'make', 'model']]
+    return final_data
+
+def clean_title(data: pd.DataFrame):
+    print('Cleaning label column')
+    data['label'] = data['label'].apply(lambda x: re.sub(r'[^a-zA-Z0-9\s]', '', x)) #removes symbols
+    data['year_raw'] = data['label'].str.extract(r'\b(\d{4})\b') #takes 4 digit numbers 
+    data['year_raw'] = pd.to_numeric(data['year_raw'], errors='coerce').astype('Int64') #turn values to float then int64
+    data['year'] = data['year_raw'].where((data['year_raw'] >=  MIN_YEAR) & (data['year_raw'] <= CURR_YEAR)) #validating year
+
+    pattern = '|'.join(CAR_MODELS)
+    data['make'] = data['label'].str.extract(f'({pattern})', flags=re.IGNORECASE) #extracting make
+    data['model'] = data['label'].str.replace(pattern, repl='', case=False, regex=True) #removing the make from the model
+    data['model'] = data['label'].str.replace(r'\b(\d{4})\b', '', case=False, regex=True) #removing the year from the model
+
+
+
